ff5e194e2e761227f1610cba04ba8c8e01149339
Baelir100 <82098614+Baelir100@users.noreply.github.com>
2021-09-17 22:28:07 +0300

    Update example.e2e.js

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,10 +1,8 @@
-const { default: AllureReporter } = require('@wdio/allure-reporter');
 const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
 
 describe('My First Login application Test', () => {
     it('should login with valid credentials', async () => {
-        AllureReporter.addStep("Open login page");
         await LoginPage.open();
 
         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
